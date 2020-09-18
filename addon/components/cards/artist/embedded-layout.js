5e7c67524a9e0a4eb2d0c290ad825700712d47bb
Burcu Noyan <burcunoyan@gmail.com>
2020-09-18 19:30:46 -0400

    Add artist card

---
+++ addon/components/cards/artist/embedded-layout.js
@@ -0,0 +1,49 @@
+import Component from '@glimmer/component';
+
+const HEADER_FIELDS = {
+  "title": "title",
+  "description": "role",
+  "image": "img-url"
+};
+
+const VIEW_FIELDS = [
+  "email",
+  "website",
+  "recordings"
+];
+
+export default class EmbeddedLayout extends Component {
+  header = HEADER_FIELDS;
+  viewFields = VIEW_FIELDS;
+
+  get isViewMode() {
+    if (!this.args.mode) { return null; }
+    return this.args.mode === 'view' || this.args.mode === 'layout';
+  }
+
+  get headerFields() {
+    if (!this.args.card || !this.args.card.attributes || !this.header) { return null; }
+    const fieldNameMap = this.header;
+    const fields = this.args.card.attributes;
+
+    return {
+      title: fields[fieldNameMap.title],
+      description: fields[fieldNameMap.description],
+      image: fields[fieldNameMap.image]
+    }
+  }
+
+  get detailFields() {
+    if (!this.viewFields || !this.args.fields) { return null; }
+    const fieldNames = this.viewFields;
+    let fields = [];
+
+    for (let field of fieldNames) {
+      if (this.args.card.attributes[field]) {
+        fields = [...fields, this.args.fields.find(f => f.name === field)];
+      }
+    }
+
+    return fields;
+  }
+}
