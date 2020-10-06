79694eb30a147e5c47b5c676f63dc5afd6a01570
Burcu Noyan <burcunoyan@gmail.com>
2020-10-06 18:31:37 -0400

    Update sample cards

--- app/components/cards/master-recording/isolated-layout.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'fuga-v2-cards/components/cards/master-recording/isolated-layout';
--- app/components/cards/participant/embedded-layout.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'fuga-v2-cards/components/cards/participant/embedded-layout';
